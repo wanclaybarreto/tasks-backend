e090894db1db978b328cf7c397f9138df11e74de
Wanclay Barreto <wanclaybarreto@gmail.com>
2026-04-28 16:33:58 -0300

    feat: exposição dos repositórios, com exceção do de usuários, com o spring data rest; exposição dos ids dos registros nos repositórios expostos.

--- src/main/java/wb/tasks_backend/TasksBackendApplication.java
+++ src/main/java/wb/tasks_backend/TasksBackendApplication.java
@@ -2,12 +2,22 @@ package wb.tasks_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import wb.tasks_backend.domain.Task;
+import wb.tasks_backend.domain.User;
 
 @SpringBootApplication
-public class TasksBackendApplication {
+public class TasksBackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksBackendApplication.class, args);
 	}
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Task.class);
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+    }
 }
--- src/main/java/wb/tasks_backend/domain/UserRepository.java
+++ src/main/java/wb/tasks_backend/domain/UserRepository.java
@@ -1,9 +1,11 @@
 package wb.tasks_backend.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
